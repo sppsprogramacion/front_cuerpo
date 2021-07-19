94f08b3f8985a3fdf8998cdde1624ed581f61d04
Fernando Salva <xxavierargentino@hotmail.com>
2021-07-19 20:57:37 -0300

    validaciones en el formulario de edicion de personal

--- src/app/pages/edit/edit.component.ts
+++ src/app/pages/edit/edit.component.ts
@@ -24,11 +24,11 @@ export class EditComponent implements OnInit {
     this.forma = this.fb.group({
        id_personal: [this.dataEdit.id_personal,Validators.required],
        apellido_1: [this.dataEdit.apellido_1,Validators.required],
-       apellido_2: [this.dataEdit.apellido_2,Validators.required],
+       apellido_2: [this.dataEdit.apellido_2],
        nombre_1: [this.dataEdit.nombre_1,Validators.required],
-       nombre_2: [this.dataEdit.nombre_2,Validators.required],
-       nombre_3: [this.dataEdit.nombre_3,Validators.required],
-       dni: [this.dataEdit.dni,Validators.required]
+       nombre_2: [this.dataEdit.nombre_2],
+       nombre_3: [this.dataEdit.nombre_3],
+       dni: [this.dataEdit.dni,[Validators.required,Validators.min(1111111),Validators.max(99999999)]]
   });
     this.submitForm();
    }
@@ -37,8 +37,25 @@ export class EditComponent implements OnInit {
     
   }
 
+  get apellido1NoValido(){
+    return this.forma.get('apellido_1')?.invalid && this.forma.get('apellido_1')?.touched;
+  }
+
+  get nombre1NoValido(){
+    return this.forma.get('nombre_1')?.invalid && this.forma.get('nombre_1')?.touched;
+  }
+
+  get dniNoValido(){
+    return this.forma.get('dni')?.invalid && this.forma.get('dni')?.touched;
+  }
+
+  
+
   submitForm(){
     console.log('DATOS EN FORMA:', this.forma);
+    if(this.forma.invalid){
+      return Object.values(this.forma.controls).forEach(control => control.markAsTouched());
+    }
   }
 
   
